597d657b20ddeeab51a960a9c9eaaf633e42bee2
TamaraKoval <kovalpsy@mail.ru>
2023-03-28 01:54:49 +0300

    Facade done

--- src/main/java/BinOps.java
+++ src/main/java/BinOps.java
@@ -1,2 +1,26 @@
 public class BinOps {
+
+    public String sum(String a, String b) throws NumberFormatException {
+        String result = null;
+        try {
+            int aBin = Integer.parseInt(a, 2);
+            int bBin = Integer.parseInt(b, 2);
+            result =  Integer.toBinaryString(aBin + bBin);
+        } catch (NumberFormatException exception) {
+            System.out.println("Ошибка вычислений: Неподходящий аргумент");
+        }
+        return result;
+    }
+
+    public String mult(String a, String b) {
+        String result = null;
+        try {
+            int aBin = Integer.parseInt(a, 2);
+            int bBin = Integer.parseInt(b, 2);
+            result =  Integer.toBinaryString(aBin * bBin);
+        } catch (NumberFormatException exception) {
+            System.out.println("Ошибка вычислений: Неподходящий аргумент");
+        }
+        return result;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.sum("101", "110")); // 5 + 6 = 11(10), т.е. 1011(2);
+
+        System.out.println(bins.mult("101", "110")); // 5 * 6 = 10(10), т.е. 11110(2)
+
+        System.out.println(bins.sum("8", "10")); // неподходящий аргумент
+    }
 }
